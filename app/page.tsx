625544d9a0ae2997fd5ffc63037d727d33bc3cb5
divik10 <diviksatija99@gmail.com>
2026-02-28 00:57:33 +0530

    fix clerk auth async in home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
 export default async function Home() {
-  const { userId } = auth();
+  const { userId } = await auth();
 
   if (userId) {
     redirect("/dashboard");
